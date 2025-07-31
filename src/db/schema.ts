a95f873a25c71e28c208d9f11933772865c0ac78
Monis <monissms16@gmail,com>
2025-08-01 02:05:05 +0530

    Implement meetings feature with CRUD operations and UI components

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,8 @@
 import { nanoid } from "nanoid"
-import { text, pgTable,timestamp,boolean } from "drizzle-orm/pg-core";
+import { text, pgTable,timestamp,boolean, pgEnum } from "drizzle-orm/pg-core";
 import { agentsInsertSchema } from "@/modules/agents/schemas";
 import { z } from "zod";
+
 
 export const user = pgTable("user", {
 id: text('id').primaryKey(),
@@ -66,4 +67,35 @@ export const agents = pgTable("agents",{
 
 export const agentsUpdateSchema = agentsInsertSchema.extend({
      id: z.string().min(1, { message:"ID is required" }),
+})
+
+export const meetingStatus = pgEnum("meeting_status", [
+  "upcoming",
+  "active"  ,
+     "completed",
+     "cancelled",
+     "processing", 
+]);
+export const meetings = pgTable("meetings",{
+     id: text("id")
+     .primaryKey()
+     .$defaultFn( ()=> nanoid()),
+     name: text("name").notNull(),
+     userId: text("user_id")
+     .notNull()
+     .references(() => user.id, {onDelete: "cascade" }),
+
+     agentId: text("agent_id")
+     .notNull()
+     .references(() => agents.id, {onDelete: "cascade" }),
+     status: meetingStatus("status").notNull().default("upcoming"),
+      startedAt: timestamp("started_at"),
+     endedAt: timestamp("ended_at"),
+     transcriptUrl: text("transcript_url"),
+     recordingUrl: text("recording_url"),
+     summary: text("summary"),
+
+     createdAt: timestamp("created_at").notNull().defaultNow(),
+     updatedAt: timestamp("updated_at").notNull().defaultNow(),
+
 })
