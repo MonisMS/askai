6b46dc239d829637723b51a3ddf750448e8a595a
Monis <monissms16@gmail,com>
2025-08-02 21:47:18 +0530

    added meetings data table

--- src/modules/meetings/server/procedure.ts
+++ src/modules/meetings/server/procedure.ts
@@ -1,35 +1,39 @@
 import { db } from "@/db";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { meetings } from "@/db/schema";
+import { meetings, agents } from "@/db/schema";
 import { z } from "zod";
-import { and,count,desc,eq, getTableColumns, ilike } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
 import { sql } from "drizzle-orm";
-import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
+import {
+  DEFAULT_PAGE,
+  DEFAULT_PAGE_SIZE,
+  MAX_PAGE_SIZE,
+  MIN_PAGE_SIZE,
+} from "@/constants";
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "./schemas";
 
 export const meetingsRouter = createTRPCRouter({
+  update: protectedProcedure
+    .input(meetingsUpdateSchema)
+    .mutation(async ({ ctx, input }) => {
+      const [updatedMeeting] = await db
+        .update(meetings)
+        .set(input)
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        )
+        .returning();
+      if (!updatedMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
+      return updatedMeeting;
+    }),
 
-update: protectedProcedure
-  .input(meetingsUpdateSchema)
-  .mutation(async({ ctx, input}) => {
-const [updatedMeeting] = await db
-    .update(meetings)
-    .set(input)
-    .where(
-      and(
-        eq(meetings.id, input.id),
-        eq(meetings.userId, ctx.auth.user.id)
-      )
-    )
-    .returning();
-    if(!updatedMeeting) {
-      throw new TRPCError({ code: "NOT_FOUND", message: "Meeting not found" });
-    }
-    return updatedMeeting;
-  }),
-
-create: protectedProcedure
+  create: protectedProcedure
     .input(meetingsInsertSchema)
     .mutation(async ({ input, ctx }) => {
       const [createdMeeting] = await db
@@ -40,29 +44,28 @@ create: protectedProcedure
         })
         .returning();
 
-        //TODO Create stream call, usert stream users
+      //TODO Create stream call, usert stream users
       return createdMeeting;
     }),
 
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
-    .query(async ({ input,ctx }) => {
+    .query(async ({ input, ctx }) => {
       const [existingMeeting] = await db
         .select({
-         
           ...getTableColumns(meetings),
         })
         .from(meetings)
         .where(
-          and(
-            eq(meetings.id, input.id),
-            eq(meetings.userId, ctx.auth.user.id)
-          )
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
         );
 
-            if(!existingMeeting) {
-              throw new TRPCError({ code: "NOT_FOUND", message: "Meeting not found" });
-            }
+      if (!existingMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
 
       return existingMeeting;
     }),
@@ -70,52 +73,55 @@ create: protectedProcedure
   getMany: protectedProcedure
     .input(
       z.object({
-          page: z.number().default(DEFAULT_PAGE),
-          pageSize: z.number()
-            .min(MIN_PAGE_SIZE)
-            .max(MAX_PAGE_SIZE)
-            .default(DEFAULT_PAGE_SIZE),
-          search: z.string().nullish(),
-        })
-        
+        page: z.number().default(DEFAULT_PAGE),
+        pageSize: z
+          .number()
+          .min(MIN_PAGE_SIZE)
+          .max(MAX_PAGE_SIZE)
+          .default(DEFAULT_PAGE_SIZE),
+        search: z.string().nullish(),
+      })
     )
     .query(async ({ ctx, input }) => {
-        const {search, page, pageSize} = input;
+      const { search, page, pageSize } = input;
       const data = await db
         .select({
           meetingCount: sql<number>`5`,
           ...getTableColumns(meetings),
+          agent: agents,
+          duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as(
+            "duration"
+          ),
         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
-            and(
-                eq(meetings.userId, ctx.auth.user.id),
-                search ? ilike(meetings.name, `%${search}%`) : undefined
-            )
+          and(
+            eq(meetings.userId, ctx.auth.user.id),
+            search ? ilike(meetings.name, `%${search}%`) : undefined
+          )
         )
-        .orderBy(desc(meetings.createdAt),desc(meetings.id))
+        .orderBy(desc(meetings.createdAt), desc(meetings.id))
         .limit(pageSize)
         .offset((page - 1) * pageSize);
 
-        const [total] = await db
-        .select({count: count()})
+      const [total] = await db
+        .select({ count: count() })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
-            and(
-                eq(meetings.userId, ctx.auth.user.id), 
-                search ? ilike(meetings.name, `%${search}%`) : undefined
-            )
+          and(
+            eq(meetings.userId, ctx.auth.user.id),
+            search ? ilike(meetings.name, `%${search}%`) : undefined
+          )
         );
 
-        const totalPages = Math.ceil(total.count / pageSize);
+      const totalPages = Math.ceil(total.count / pageSize);
 
-        return {
-          items: data,
-          total: total.count,
-          totalPages,
-        };  
-      
+      return {
+        items: data,
+        total: total.count,
+        totalPages,
+      };
     }),
-
-
 });
--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,16 +1,29 @@
 "use client"
 
 import { useTRPC } from "@/trpc/client"
+import { DataTable } from "@/components/data-table";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
-
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 export const MeetingsView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
     return(
-        <div className="overflow-x-scroll">
-            {JSON.stringify(data)}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+           <DataTable
+           data={data.items}
+           columns={ columns }
+           />
+            {data.items.length === 0 && (
+                 
+                   <EmptyState
+                     title="Create your first meeting"
+                     description="Schedule a meeting to connect with other. Each meeting
+                     lets you collaborate, share ideas, and interact with participants in real time."
+                   />
+               )}
         </div>
     )
 }
