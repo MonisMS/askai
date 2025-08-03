c739a0fbf86a5cb07ecb804ae496292a70643f1b
Monis <monissms16@gmail,com>
2025-08-03 13:12:56 +0530

    add meeting page

---
+++ src/modules/meetings/ui/components/meeting-id-view-header.tsx
@@ -0,0 +1,77 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
+import {
+  ChevronRightIcon,
+  TrashIcon,
+  PencilIcon,
+  MoreVerticalIcon,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+interface Props {
+  meetingId: string;
+  meetingName: string;
+  onEdit: () => void;
+  onRemove: () => void;
+}
+
+export const MeetingIdViewHeader = ({
+meetingId,
+  meetingName,
+  onEdit,
+  onRemove,
+}: Props) => {
+  return (
+    <div className="flex items-center justify-between">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className="font-medium text-xl">
+              <Link href={`/meetings`}>My Meetings</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator className="text-foreground text-xl font-medium [&>svg]:size-4">
+            <ChevronRightIcon />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbLink
+              asChild
+              className="font-medium text-xl text-foreground"
+            >
+              <Link href={`/agents/${meetingId}`}>{meetingName}</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {/*Without modal={false},the dialogue this dropdown opens causes the website to get unclickable  */}
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost">
+            <MoreVerticalIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={onEdit}>
+            <PencilIcon className="text-black size-4" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={onRemove}>
+            <TrashIcon className="text-black size-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
